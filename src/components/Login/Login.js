ab95f46bb087e900b22f0f82dbc2a13fd46d3755
Alev-Narcin <alevnarcin@outlook.com>
2023-02-05 14:08:18 +0300

    Building & Using a Custom Context Provider Component

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,8 +1,9 @@
-import React, {useEffect, useReducer, useState} from 'react';
+import React, {useContext, useEffect, useReducer, useState} from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
+import {AuthContext} from "../../store/auth-context";
 
 const emailReducer = (state, action) => {
     if (action.type === 'USER_INPUT') {
@@ -24,11 +25,7 @@ const passwordReducer = (state, action) => {
     return {value: '', isValid: false};
 }
 
-const Login = (props) => {
-    // const [enteredEmail, setEnteredEmail] = useState('');
-    // const [emailIsValid, setEmailIsValid] = useState();
-    // const [enteredPassword, setEnteredPassword] = useState('');
-    // const [passwordIsValid, setPasswordIsValid] = useState();
+const Login = () => {
     const [formIsValid, setFormIsValid] = useState(false);
 
     const [emailState, dispatchEmail] = useReducer(emailReducer, {value: '', isValid: false});
@@ -72,8 +69,10 @@ const Login = (props) => {
 
     const submitHandler = (event) => {
         event.preventDefault();
-        props.onLogin(emailState.value, passwordState.value);
+        authCtx.onLogin(emailState.value, passwordState.value);
     };
+
+    const authCtx = useContext(AuthContext);
 
     return (
         <Card className={classes.login}>
