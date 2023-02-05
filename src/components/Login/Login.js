b9acfccd7faff294c8bb877f9ba3e8318752d43d
Alev-Narcin <alevnarcin@outlook.com>
2023-02-05 23:07:42 +0300

    Diving into "Forward Refs"

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,4 +1,4 @@
-import React, {useContext, useEffect, useReducer, useState} from 'react';
+import React, {useContext, useEffect, useReducer, useRef, useState} from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
@@ -33,6 +33,19 @@ const Login = () => {
 
     const [passwordState, dispatchPassword] = useReducer(passwordReducer, {value: '', isValid: false});
 
+    const authCtx = useContext(AuthContext);
+
+    const emailInputRef = useRef();
+    const passwordInputRef = useRef();
+
+    useEffect(() => {
+        console.log('EFFECT RUNNING');
+
+        return () => {
+            console.log('EFFECT CLAENUP');
+        }
+    }, []);
+
     const {isValid: emailIsValid} = emailState;
     const {isValid: passwordIsValid} = passwordState;
 
@@ -50,14 +63,10 @@ const Login = () => {
 
     const emailChangeHandler = (event) => {
         dispatchEmail({type: 'USER_INPUT', val: event.target.value});
-
-        // setFormIsValid(event.target.value.includes('@') && passwordState.isValid);
     };
 
     const passwordChangeHandler = (event) => {
         dispatchPassword({type: 'USER_INPUT', val: event.target.value})
-
-        // setFormIsValid(emailState.isValid && event.target.value.trim().length > 6);
     };
 
     const validateEmailHandler = () => {
@@ -70,40 +79,44 @@ const Login = () => {
 
     const submitHandler = (event) => {
         event.preventDefault();
-        authCtx.onLogin(emailState.value, passwordState.value);
+        if (formIsValid) {
+            authCtx.onLogin(emailState.value, passwordState.value);
+        } else if (!emailIsValid) {
+            emailInputRef.current.focus();
+        }else {
+            passwordInputRef.current.focus();
+        }
     };
 
-    const authCtx = useContext(AuthContext);
-
-    return (
-        <Card className={classes.login}>
-            <form onSubmit={submitHandler}>
-                <Input
-                    id="email"
-                    label="E-Mail"
-                    type="email"
-                    isValid={emailIsValid}
-                    value={emailState.value}
-                    onChange={emailChangeHandler}
-                    onBlur={validateEmailHandler}
-                />
-                <Input
-                    id="password"
-                    label="Password"
-                    type="password"
-                    isValid={passwordIsValid}
-                    value={passwordState.value}
-                    onChange={passwordChangeHandler}
-                    onBlur={validatePasswordHandler}
-                />
-                <div className={classes.actions}>
-                    <Button type="submit" className={classes.btn} disabled={!formIsValid}>
-                        Login
-                    </Button>
-                </div>
-            </form>
-        </Card>
-    );
+    return (<Card className={classes.login}>
+        <form onSubmit={submitHandler}>
+            <Input
+                ref={emailInputRef}
+                id="email"
+                label="E-Mail"
+                type="email"
+                isValid={emailIsValid}
+                value={emailState.value}
+                onChange={emailChangeHandler}
+                onBlur={validateEmailHandler}
+            />
+            <Input
+                ref={passwordInputRef}
+                id="password"
+                label="Password"
+                type="password"
+                isValid={passwordIsValid}
+                value={passwordState.value}
+                onChange={passwordChangeHandler}
+                onBlur={validatePasswordHandler}
+            />
+            <div className={classes.actions}>
+                <Button type="submit" className={classes.btn}>
+                    Login
+                </Button>
+            </div>
+        </form>
+    </Card>);
 };
 
 export default Login;
